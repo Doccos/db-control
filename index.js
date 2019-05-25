8c64c4a805ebbfbf351585824b8994f0817d7c02
Doccos <git@adad.at>
2019-05-25 17:45:40 +0200

    init2

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 'use strict'
 
-asdert
+let control = require("./lib/control");
+let table = require("./lib/table");
+let varchar = require("./lib/columns/varchar");
+
+module.exports.control = function (dbName, mysql) {
+    return new control.start(dbName, mysql)
+}
+
+module.exports.newTable = function (tableName) {
+    return new table.table(tableName);
+}
+
+module.exports.varChar = function (length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey) {
+    return new varchar(length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey);
+}
+
+

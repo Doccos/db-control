cdfd239861bfc4f6c807ecca63aa17a48315abee
Doccos <git@adad.at>
2019-05-29 20:27:58 +0200

    first version

--- index.js
+++ index.js
@@ -3,17 +3,21 @@
 let control = require("./lib/control");
 let table = require("./lib/table");
 let varchar = require("./lib/columns/varchar");
+let int = require("./lib/columns/int");
 
 module.exports.control = function (dbName, mysql) {
     return new control.start(dbName, mysql)
 }
 
 module.exports.newTable = function (tableName) {
-    return new table.table(tableName);
+    return new table(tableName);
 }
 
-module.exports.varChar = function (length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey) {
-    return new varchar(length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey);
+module.exports.varChar = function (name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey) {
+    return new varchar(name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey);
+}
+module.exports.int = function (name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey, isAutoIncrement) {
+    return new int(name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey, isAutoIncrement);
 }
 
 

4be151f4edf68858eaf5eb3ca8a6e7e73d43e895
Doccos <git@adad.at>
2019-05-30 20:59:13 +0200

    next step

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let control = require("./lib/control");
 let table = require("./lib/table");
 let varchar = require("./lib/columns/varchar");
 let int = require("./lib/columns/int");
-
+let timestamp = require("./lib/columns/timestamp");
+let mainColumn = require("./lib/columns/mainColumn")
 module.exports.control = function (dbName, mysql) {
     return new control.start(dbName, mysql)
 }
@@ -14,10 +15,20 @@ module.exports.newTable = function (tableName) {
 }
 
 module.exports.varChar = function (name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey) {
-    return new varchar(name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey);
+    let r = new mainColumn("varchar", name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey)
+    return r;
 }
 module.exports.int = function (name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey, isAutoIncrement) {
-    return new int(name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey, isAutoIncrement);
+    let r = new mainColumn("int", name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey);
+    r.isAutoIncrement = isAutoIncrement
+    return r;
+}
+
+module.exports.timestamp = function (name, defaultVal, isPrimaryKey,currentTS, onUpdtCurrTS) {
+    let r = new mainColumn("timestamp", name, null, false, defaultVal, false, isPrimaryKey);
+    r.currentTS = currentTS;
+    r.onUpdCurTS = onUpdtCurrTS;
+    return r;
 }
 
 

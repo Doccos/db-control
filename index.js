2f679647a8e2ab0d9f2410ba5c91e66b7b053f12
Doccos <git@adad.at>
2019-06-02 09:24:06 +0200

    Primery key removed

--- index.js
+++ index.js
@@ -11,37 +11,37 @@ module.exports.newTable = function (tableName) {
     return new table(tableName);
 }
 
-module.exports.varChar = function (name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey) {
-    let r = new mainColumn("varchar", name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey)
+module.exports.varChar = function (name, length, isNullAllowed, defaultVal, isUnsigned) {
+    let r = new mainColumn("varchar", name, length, isNullAllowed, defaultVal, isUnsigned)
     return r;
 }
-module.exports.int = function (name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey, isAutoIncrement) {
-    let r = new mainColumn("int", name, length, isNullAllowed, defaultVal, isUnsigned, isPrimaryKey);
+module.exports.int = function (name, length, isNullAllowed, defaultVal, isUnsigned,  isAutoIncrement) {
+    let r = new mainColumn("int", name, length, isNullAllowed, defaultVal, isUnsigned);
     r.isAutoIncrement = isAutoIncrement
     return r;
 }
 
-module.exports.timestamp = function (name, defaultVal, isPrimaryKey,currentTS, onUpdtCurrTS) {
-    let r = new mainColumn("timestamp", name, null, false, defaultVal, false, isPrimaryKey);
+module.exports.timestamp = function (name, defaultVal, currentTS, onUpdtCurrTS) {
+    let r = new mainColumn("timestamp", name, null, false, defaultVal, false);
     r.currentTS = currentTS;
     r.onUpdCurTS = onUpdtCurrTS;
     return r;
 }
 
-module.exports.datetime = function (name, defaultVal, isPrimaryKey,currentTS, onUpdtCurrTS) {
-    let r = new mainColumn("datetime", name, null, false, defaultVal, false, isPrimaryKey);
+module.exports.datetime = function (name, defaultVal, currentTS, onUpdtCurrTS) {
+    let r = new mainColumn("datetime", name, null, false, defaultVal, false);
     r.currentTS = currentTS;
     r.onUpdCurTS = onUpdtCurrTS;
     return r;
 }
-module.exports.date = function (name, defaultVal, isPrimaryKey,currentTS, onUpdtCurrTS) {
-    let r = new mainColumn("date", name, null, false, defaultVal, false, isPrimaryKey);
+module.exports.date = function (name, defaultVal, currentTS, onUpdtCurrTS) {
+    let r = new mainColumn("date", name, null, false, defaultVal, false);
     r.currentTS = currentTS;
     r.onUpdCurTS = onUpdtCurrTS;
     return r;
 }
-module.exports.time = function (name, defaultVal, isPrimaryKey,currentTS, onUpdtCurrTS) {
-    let r = new mainColumn("time", name, null, false, defaultVal, false, isPrimaryKey);
+module.exports.time = function (name, defaultVal, currentTS, onUpdtCurrTS) {
+    let r = new mainColumn("time", name, null, false, defaultVal, false);
     r.currentTS = currentTS;
     r.onUpdCurTS = onUpdtCurrTS;
     return r;

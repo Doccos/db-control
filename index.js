66bfcfc851e799543642d681584e22811a02ca92
Doccos <git@adad.at>
2019-06-02 09:11:22 +0200

    Alpha

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 
 let control = require("./lib/control");
 let table = require("./lib/table");
-let varchar = require("./lib/columns/varchar");
-let int = require("./lib/columns/int");
-let timestamp = require("./lib/columns/timestamp");
 let mainColumn = require("./lib/columns/mainColumn")
 module.exports.control = function (dbName, mysql) {
     return new control.start(dbName, mysql)
@@ -26,6 +23,25 @@ module.exports.int = function (name, length, isNullAllowed, defaultVal, isUnsign
 
 module.exports.timestamp = function (name, defaultVal, isPrimaryKey,currentTS, onUpdtCurrTS) {
     let r = new mainColumn("timestamp", name, null, false, defaultVal, false, isPrimaryKey);
+    r.currentTS = currentTS;
+    r.onUpdCurTS = onUpdtCurrTS;
+    return r;
+}
+
+module.exports.datetime = function (name, defaultVal, isPrimaryKey,currentTS, onUpdtCurrTS) {
+    let r = new mainColumn("datetime", name, null, false, defaultVal, false, isPrimaryKey);
+    r.currentTS = currentTS;
+    r.onUpdCurTS = onUpdtCurrTS;
+    return r;
+}
+module.exports.date = function (name, defaultVal, isPrimaryKey,currentTS, onUpdtCurrTS) {
+    let r = new mainColumn("date", name, null, false, defaultVal, false, isPrimaryKey);
+    r.currentTS = currentTS;
+    r.onUpdCurTS = onUpdtCurrTS;
+    return r;
+}
+module.exports.time = function (name, defaultVal, isPrimaryKey,currentTS, onUpdtCurrTS) {
+    let r = new mainColumn("time", name, null, false, defaultVal, false, isPrimaryKey);
     r.currentTS = currentTS;
     r.onUpdCurTS = onUpdtCurrTS;
     return r;
